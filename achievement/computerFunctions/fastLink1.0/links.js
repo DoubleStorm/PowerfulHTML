2183b50d59913885c592331097c82e11cf627e11
DESKTOP-9C2C69T\何沁 <2570679716@qq.com>
2020-09-07 13:33:20 +0800

    更新link1.0及link2.0功能页面

---
+++ achievement/computerFunctions/fastLink1.0/links.js
@@ -0,0 +1,126 @@
+var theLinks = {
+    /* target 取值 self、_blank */
+    target: 'self',
+    /*
+        {name:"",link:"",describe:""},
+     
+        {
+            name:"",
+            links:[
+                {name:"",link:""},
+                {name:"",link:""}
+            ]
+        },
+    */
+    links: {
+        goodLinks:[
+            {name:"stackoverflow",link:"https://stackoverflow.com/",describe:"这么说吧，一边改 bug 一边学英语"},
+            {name:"goobe",link:"https://goobe.io/",describe:"面向程序员的搜索引擎"},
+            {name:"Bing",link:"https://cn.bing.com/?FORM=BEHPTB",describe:"微软 Bing 搜索"},
+            {name:"百度",link:"https://baike.baidu.com/"},
+            {name:"菜鸟工具",link:"https://c.runoob.com/",describe:"多种工具"},
+            {name:"TOOLFK",link:"https://www.toolfk.com/",describe:"各种工具"},
+            {name:"资源狗",link:"http://www.ziyuangou.com/",describe:"写这个网页的灵感来源"},
+            {name:"找到 AI",link:"https://zhaodao.ai/",describe:"用AI来找点乐子"}
+        ],
+        goldLinks:[{
+            name:"生活社区",
+            links:[
+                {name:"哔哩哔哩",link:"https://www.bilibili.com/"},
+                {name:"微博",link:"https://weibo.com/"},
+                {name:"知乎",link:"https://www.zhihu.com/"},
+                {name:"场库",link:"https://www.vmovier.com/"}
+            ]
+        },{
+            name:"持续好好学习",
+            links:[
+                {name:"微信读书",link:"https://weread.qq.com/"},
+                {name:"TED",link:"https://www.ted.com/#/"},
+                {name:"慕课",link:"https://www.icourse163.org/"},
+                {name:"网易公开课",link:"https://open.163.com/"}
+            ]
+        },{
+            name:"技术交流社区",
+            links:[
+                {name:"GitHub",link:"https://github.com/"},
+                {name:"npm 官网",link:"https://www.npmjs.com/"},
+                {name:"力扣",link:"https://leetcode-cn.com/problemset/all/"},
+                {name:"牛客网",link:"https://www.nowcoder.com/"},
+                {name:"CSDN",link:"https://www.csdn.net"},
+                {name:"菜鸟教程",link:"https://www.runoob.com/"},
+                {name:"W3school",link:"https://www.w3school.com.cn/index.html"},
+                {name:"源码之家",link:"https://www.mycodes.net/"},
+                {name:"w3cschool",link:"https://www.w3cschool.cn/"},
+                {name:"how2j.cn",link:"https://how2j.cn/"}
+            ]
+        },{
+            name:"技术学习",
+            links:[
+                {name:"优质的中文前端博客",link:"https://github.com/FrankFang/best-chinese-front-end-blogs"},
+                {name:"廖雪峰的官方网站",link:"https://www.liaoxuefeng.com/"},
+                {name:"柳婼 の blog",link:"https://www.liuchuo.net/"}
+            ]
+        },{
+            name:"偶尔打打游戏",
+            links:[
+                {name:"HTML5 games",link:"https://html5games.com/"},
+                {name:"游侠网",link:"https://www.ali213.net/"},
+                {name:"4399小游戏",link:"http://www.4399.com/"}
+            ]
+        },{
+            name:"看电影",
+            links:[
+                {name:"80s手机电影网",link:"https://www.80s.tw/"},
+                {name:"80S电影网",link:"http://80smp4.cc/"},
+                {name:"电影天堂",link:"http://www.kk2w1.com/"}
+            ]
+        },{
+            name:"看动漫",
+            links:[
+                {name:"奇奇动漫",link:"https://www.qiqidongman.com/"}
+            ]
+        },{
+            name:"求职",
+            links:[
+                {name:"BOSS直聘",link:"https://www.zhipin.com/"},
+                {name:"新职业",link:"https://www.ncss.cn/"}
+            ]
+        },{
+            name:"网上接单",
+            links:[
+                {name:"猪八戒网",link:"https://nanchang.zbj.com/"},
+                {name:"一品威客",link:"https://www.epwk.com/"},
+                {name:"猿急送",link:"https://www.yuanjisong.com/"}
+            ]
+        },{
+            name:"租房",
+            links:[
+                {name:"自如",link:"http://www.ziroom.com/"}
+            ]
+        },{
+            name:"线上商城",
+            links:[
+                {name:"淘宝网",link:"https://www.taobao.com/"},
+                {name:"天猫",link:"https://www.tmall.com/"},
+                {name:"京东",link:"https://www.jd.com/"},
+                {name:"亚马逊",link:"https://www.amazon.cn/s/ref=nb_sb_noss"}
+            ]
+        },{
+            name:"在线翻译",
+            links:[
+                {name:"百度翻译",link:"https://fanyi.baidu.com/translate"}
+            ]
+        },{
+            name:"地图",
+            links:[
+                {name:"百度地图",link:"https://map.baidu.com/"}
+            ]
+        },{
+            name:"学术交流",
+            links:[
+                {name:"中国知网",link:"https://www.cnki.net/"},
+                {name:"百度学术",link:"https://xueshu.baidu.com/"}
+            ]
+        }]
+    }
+}
---
+++ achievement/computerFunctions/fastLink2.0/links.js
@@ -0,0 +1,58 @@
+var theLinks = {
+    target: 'self',
+    links: [
+        /*
+            ,{name:"",link:"",tip:""}
+        */
+        [
+            {name:"Angular 中文网",link:"https://angular.cn/docs"}
+            ,{name:"NG-ZORRO 官网",link:"https://ng.ant.design/docs/introduce/zh"}
+            ,{name:"Echarts 官网",link:"https://echarts.apache.org/examples/zh/index.html"}
+            ,{name:"ag-Grid 官网",link:"https://www.ag-grid.com/example.php#/"}
+            ,{name:"RxJS 中文文档",link:"https://cn.rx.js.org/manual/overview.html"}
+            ,{name:"Sass 中文文档",link:"https://www.sass.hk/docs/"}
+            ,{name:"Highcharts 教程",link:"https://www.highcharts.com.cn/docs/start-introduction"}
+            ,{name:"jqcloud 教程",link:"https://blog.csdn.net/qq_21895079/article/details/76853384"}
+            ,{name:"jSignature 教程",link:"https://blog.csdn.net/liansehai/article/details/80255294"}
+            ,{name:"node 中文网",link:"https://www.npmjs.cn/"}
+            ,{name:"MDN Web docs",link:"https://developer.mozilla.org/zh-CN/",tip:"源于开发者，服务开发者"}
+        ],[
+            {name:"Angular Update Guide",link:"https://update.angular.io/",tip:"Angular 项目升级指导"}
+            ,{name:"BootStrap 布局工具",link:"https://www.runoob.com/try/bootstrap/layoutit/",tip:"快速获得 BootStrap 布局结构代码"}
+            ,{name:"BootStrap 图标工具",link:"https://www.runoob.com/try/demo_source/bootstrap-glyph-customization.htm",tip:"快速获得 BootStrap 图标"}
+            ,{name:"HTML 字符实体转换",link:"https://c.runoob.com/front-end/691",tip:"快速将 HTML 代码中的特殊符号转换成字符实体"}
+            ,{name:"CSS 拾色器",link:"https://www.runoob.com/tags/html-colorpicker.html"}
+            ,{name:"FontAwesome 参考",link:"https://www.runoob.com/font-awesome/fontawesome-reference.html",tip:"FontAwesome 图标使用参考"}
+            ,{name:"Animate.css 参考",link:"http://www.jq22.com/yanshi819",tip:"Animate.css 动画库使用参考"}
+            ,{name:"Can I use",link:"https://caniuse.com/",tip:"查看 W3C 标准在各浏览器上的支持程度"}
+            ,{name:"CodePen",link:"https://codepen.io/",tip:"前端代码在线编写"}
+            ,{name:"Cool Backgrounds",link:"https://coolbackgrounds.io/",tip:"炫酷背景图片生成器"}
+            ,{name:"BootStrap 模板资源网",link:"http://www.bootstrapmb.com/"}
+            ,{name:"HTML 字符实体参考",link:"https://www.w3school.com.cn/tags/html_ref_entities.html"}
+            ,{name:"HTML URL 编码字符",link:"https://www.w3school.com.cn/tags/html_ref_urlencode.html"}
+            ,{name:"菜鸟工具 SVG 在线编辑器",link:"https://c.runoob.com/more/svgeditor/"}
+        ],[
+            {name:"在线 PS",link:"https://ps.gaoding.com/#/"}
+            ,{name:"比特虫",link:"http://www.bitbug.net/",tip:"在线制作 ico 图标"}
+            ,{name:"菜鸟工具在线画图",link:"https://c.runoob.com/more/shapefly-diagram/"}
+            ,{name:"ProcessOn",link:"https://www.processon.com/",tip:"ProcessOn 在线画图工具"}
+            ,{name:"菜鸟工具汉字拼音",link:"https://c.runoob.com/front-end/5523",tip:"将汉字翻译成拼音字符"}
+            ,{name:"TinyPNG",link:"https://tinypng.com/",tip:"在线压缩图片，可支持 png、jpg 格式"}
+            ,{name:"picdiet",link:"https://www.picdiet.com/zh-cn",tip:"在线压缩图片，可支持 jpg、jpeg 格式"}
+            ,{name:"Smart Resize",link:"https://www.smartresize.com/zh-cn",tip:"在线可批量剪裁图片工具"}
+            ,{name:"Convertio",link:"https://convertio.co/zh/",tip:"强大的在线文件转换工具"}
+            ,{name:"PDF24 Tools",link:"https://tools.pdf24.org/zh/",tip:"多功能 PDF 文件在线编辑器"}
+            ,{name:"小鸡词典",link:"https://jikipedia.com/",tip:"速查当下网络流行语"}
+            ,{name:"写作猫",link:"https://xiezuocat.com/#/",tip:"简单写作手法提升"}
+            ,{name:"Pexels",link:"https://www.pexels.com/zh-cn/",tip:"高质量图片下载网站"}
+            ,{name:"CG 资源网",link:"https://www.cgown.com/",tip:"PR、AE 等资源文件下载网站"}
+            ,{name:"鸠摩搜书",link:"https://www.jiumodiary.com/",tip:"来这找书！好用！"}
+        ],[
+            {name:"HTML5 网页游戏",link:"http://html5.huceo.com/",tip:"HTML5 技术实现网页"}
+            ,{name:"WiFi Mouse",link:"http://wifimouse.necta.us/#download",tip:"手机充当电脑鼠标"}
+            ,{name:"Kick Ass",link:"https://kickassapp.com/",tip:"脑洞网页消除游戏"}
+            ,{name:"airpano",link:"https://www.airpano.com/",tip:"在线旅游"}
+            ,{name:"AlteredQualia",link:"https://alteredqualia.com/",tip:"头部模型"}
+        ]
+    ]
+}
