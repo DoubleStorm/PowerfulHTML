5454de76d8087442ffeb53e5f6099d95d3fa692b
DESKTOP-9C2C69T\何沁 <2570679716@qq.com>
2020-09-10 16:32:07 +0800

    更新标签右键点击跳转效果

--- achievement/computerFunctions/fastLink2.0/links.js
+++ achievement/computerFunctions/fastLink2.0/links.js
@@ -2,57 +2,59 @@ var theLinks = {
     target: 'self',
     links: [
         /*
-            ,{name:"",link:"",tip:""}
+            ,{name:"",leftClick:"",rightClick="",tip:"（左键中文文档，右键英文文档）"}
+            ,{name:"",leftClick:"",tip:""}
+            ,{name:"",leftClick:""}
         */
         [
-            {name:"Angular 中文网",link:"https://angular.cn/docs"}
-            ,{name:"NG-ZORRO 官网",link:"https://ng.ant.design/docs/introduce/zh"}
-            ,{name:"Echarts 官网",link:"https://echarts.apache.org/examples/zh/index.html"}
-            ,{name:"ag-Grid 官网",link:"https://www.ag-grid.com/example.php#/"}
-            ,{name:"RxJS 中文文档",link:"https://cn.rx.js.org/manual/overview.html"}
-            ,{name:"Sass 中文文档",link:"https://www.sass.hk/docs/"}
-            ,{name:"Highcharts 教程",link:"https://www.highcharts.com.cn/docs/start-introduction"}
-            ,{name:"jqcloud 教程",link:"https://blog.csdn.net/qq_21895079/article/details/76853384"}
-            ,{name:"jSignature 教程",link:"https://blog.csdn.net/liansehai/article/details/80255294"}
-            ,{name:"node 中文网",link:"https://www.npmjs.cn/"}
-            ,{name:"MDN Web docs",link:"https://developer.mozilla.org/zh-CN/",tip:"源于开发者，服务开发者"}
+            {name:"Angular 中文网",leftClick:"https://angular.cn/docs"}
+            ,{name:"NG-ZORRO 官网",leftClick:"https://ng.ant.design/docs/introduce/zh"}
+            ,{name:"Echarts 官网",leftClick:"https://echarts.apache.org/examples/zh/index.html"}
+            ,{name:"ag-Grid 官网",leftClick:"https://www.ag-grid.com/example.php#/"}
+            ,{name:"RxJS 官方教程",leftClick:"https://cn.rx.js.org/manual/overview.html",rightClick:"https://rxjs.dev/guide/overview",tip:"多学学JS异步编程的其他方式（左键中文文档，右键英文文档）"}
+            ,{name:"Sass 中文文档",leftClick:"https://www.sass.hk/docs/"}
+            ,{name:"Highcharts 官方教程",leftClick:"https://www.highcharts.com.cn/docs/start-introduction"}
+            ,{name:"jqcloud 教程",leftClick:"https://blog.csdn.net/qq_21895079/article/details/76853384"}
+            ,{name:"jSignature 教程",leftClick:"https://blog.csdn.net/liansehai/article/details/80255294"}
+            ,{name:"node 中文网",leftClick:"https://www.npmjs.cn/"}
+            ,{name:"MDN Web docs",leftClick:"https://developer.mozilla.org/zh-CN/",tip:"源于开发者，服务开发者"}
         ],[
-            {name:"Angular Update Guide",link:"https://update.angular.io/",tip:"Angular 项目升级指导"}
-            ,{name:"BootStrap 布局工具",link:"https://www.runoob.com/try/bootstrap/layoutit/",tip:"快速获得 BootStrap 布局结构代码"}
-            ,{name:"BootStrap 图标工具",link:"https://www.runoob.com/try/demo_source/bootstrap-glyph-customization.htm",tip:"快速获得 BootStrap 图标"}
-            ,{name:"HTML 字符实体转换",link:"https://c.runoob.com/front-end/691",tip:"快速将 HTML 代码中的特殊符号转换成字符实体"}
-            ,{name:"CSS 拾色器",link:"https://www.runoob.com/tags/html-colorpicker.html"}
-            ,{name:"FontAwesome 参考",link:"https://www.runoob.com/font-awesome/fontawesome-reference.html",tip:"FontAwesome 图标使用参考"}
-            ,{name:"Animate.css 参考",link:"http://www.jq22.com/yanshi819",tip:"Animate.css 动画库使用参考"}
-            ,{name:"Can I use",link:"https://caniuse.com/",tip:"查看 W3C 标准在各浏览器上的支持程度"}
-            ,{name:"CodePen",link:"https://codepen.io/",tip:"前端代码在线编写"}
-            ,{name:"Cool Backgrounds",link:"https://coolbackgrounds.io/",tip:"炫酷背景图片生成器"}
-            ,{name:"BootStrap 模板资源网",link:"http://www.bootstrapmb.com/"}
-            ,{name:"HTML 字符实体参考",link:"https://www.w3school.com.cn/tags/html_ref_entities.html"}
-            ,{name:"HTML URL 编码字符",link:"https://www.w3school.com.cn/tags/html_ref_urlencode.html"}
-            ,{name:"菜鸟工具 SVG 在线编辑器",link:"https://c.runoob.com/more/svgeditor/"}
+            {name:"Angular Update Guide",leftClick:"https://update.angular.io/",tip:"Angular 项目升级指导"}
+            ,{name:"BootStrap 布局工具",leftClick:"https://www.runoob.com/try/bootstrap/layoutit/",tip:"快速获得 BootStrap 布局结构代码"}
+            ,{name:"BootStrap 图标工具",leftClick:"https://www.runoob.com/try/demo_source/bootstrap-glyph-customization.htm",tip:"快速获得 BootStrap 图标"}
+            ,{name:"HTML 字符实体转换",leftClick:"https://c.runoob.com/front-end/691",tip:"快速将 HTML 代码中的特殊符号转换成字符实体"}
+            ,{name:"CSS 拾色器",leftClick:"https://www.runoob.com/tags/html-colorpicker.html"}
+            ,{name:"FontAwesome 参考",leftClick:"https://www.runoob.com/font-awesome/fontawesome-reference.html",tip:"FontAwesome 图标使用参考"}
+            ,{name:"Animate.css 参考",leftClick:"http://www.jq22.com/yanshi819",tip:"Animate.css 动画库使用参考"}
+            ,{name:"Can I use",leftClick:"https://caniuse.com/",tip:"查看 W3C 标准在各浏览器上的支持程度"}
+            ,{name:"CodePen",leftClick:"https://codepen.io/",tip:"前端代码在线编写"}
+            ,{name:"Cool Backgrounds",leftClick:"https://coolbackgrounds.io/",tip:"炫酷背景图片生成器"}
+            ,{name:"BootStrap 模板资源网",leftClick:"http://www.bootstrapmb.com/"}
+            ,{name:"HTML 字符实体参考",leftClick:"https://www.w3school.com.cn/tags/html_ref_entities.html"}
+            ,{name:"HTML URL 编码字符",leftClick:"https://www.w3school.com.cn/tags/html_ref_urlencode.html"}
+            ,{name:"菜鸟工具 SVG 在线编辑器",leftClick:"https://c.runoob.com/more/svgeditor/"}
         ],[
-            {name:"在线 PS",link:"https://ps.gaoding.com/#/"}
-            ,{name:"比特虫",link:"http://www.bitbug.net/",tip:"在线制作 ico 图标"}
-            ,{name:"菜鸟工具在线画图",link:"https://c.runoob.com/more/shapefly-diagram/"}
-            ,{name:"ProcessOn",link:"https://www.processon.com/",tip:"ProcessOn 在线画图工具"}
-            ,{name:"菜鸟工具汉字拼音",link:"https://c.runoob.com/front-end/5523",tip:"将汉字翻译成拼音字符"}
-            ,{name:"TinyPNG",link:"https://tinypng.com/",tip:"在线压缩图片，可支持 png、jpg 格式"}
-            ,{name:"picdiet",link:"https://www.picdiet.com/zh-cn",tip:"在线压缩图片，可支持 jpg、jpeg 格式"}
-            ,{name:"Smart Resize",link:"https://www.smartresize.com/zh-cn",tip:"在线可批量剪裁图片工具"}
-            ,{name:"Convertio",link:"https://convertio.co/zh/",tip:"强大的在线文件转换工具"}
-            ,{name:"PDF24 Tools",link:"https://tools.pdf24.org/zh/",tip:"多功能 PDF 文件在线编辑器"}
-            ,{name:"小鸡词典",link:"https://jikipedia.com/",tip:"速查当下网络流行语"}
-            ,{name:"写作猫",link:"https://xiezuocat.com/#/",tip:"简单写作手法提升"}
-            ,{name:"Pexels",link:"https://www.pexels.com/zh-cn/",tip:"高质量图片下载网站"}
-            ,{name:"CG 资源网",link:"https://www.cgown.com/",tip:"PR、AE 等资源文件下载网站"}
-            ,{name:"鸠摩搜书",link:"https://www.jiumodiary.com/",tip:"来这找书！好用！"}
+            {name:"在线 PS",leftClick:"https://ps.gaoding.com/#/"}
+            ,{name:"比特虫",leftClick:"http://www.bitbug.net/",tip:"在线制作 ico 图标"}
+            ,{name:"菜鸟工具在线画图",leftClick:"https://c.runoob.com/more/shapefly-diagram/"}
+            ,{name:"ProcessOn",leftClick:"https://www.processon.com/",tip:"ProcessOn 在线画图工具"}
+            ,{name:"菜鸟工具汉字拼音",leftClick:"https://c.runoob.com/front-end/5523",tip:"将汉字翻译成拼音字符"}
+            ,{name:"TinyPNG",leftClick:"https://tinypng.com/",tip:"在线压缩图片，可支持 png、jpg 格式"}
+            ,{name:"picdiet",leftClick:"https://www.picdiet.com/zh-cn",tip:"在线压缩图片，可支持 jpg、jpeg 格式"}
+            ,{name:"Smart Resize",leftClick:"https://www.smartresize.com/zh-cn",tip:"在线可批量剪裁图片工具"}
+            ,{name:"Convertio",leftClick:"https://convertio.co/zh/",tip:"强大的在线文件转换工具"}
+            ,{name:"PDF24 Tools",leftClick:"https://tools.pdf24.org/zh/",tip:"多功能 PDF 文件在线编辑器"}
+            ,{name:"小鸡词典",leftClick:"https://jikipedia.com/",tip:"速查当下网络流行语"}
+            ,{name:"写作猫",leftClick:"https://xiezuocat.com/#/",tip:"简单写作手法提升"}
+            ,{name:"Pexels",leftClick:"https://www.pexels.com/zh-cn/",tip:"高质量图片下载网站"}
+            ,{name:"CG 资源网",leftClick:"https://www.cgown.com/",tip:"PR、AE 等资源文件下载网站"}
+            ,{name:"鸠摩搜书",leftClick:"https://www.jiumodiary.com/",tip:"来这找书！好用！"}
         ],[
-            {name:"HTML5 网页游戏",link:"http://html5.huceo.com/",tip:"HTML5 技术实现网页"}
-            ,{name:"WiFi Mouse",link:"http://wifimouse.necta.us/#download",tip:"手机充当电脑鼠标"}
-            ,{name:"Kick Ass",link:"https://kickassapp.com/",tip:"脑洞网页消除游戏"}
-            ,{name:"airpano",link:"https://www.airpano.com/",tip:"在线旅游"}
-            ,{name:"AlteredQualia",link:"https://alteredqualia.com/",tip:"头部模型"}
+            {name:"HTML5 网页游戏",leftClick:"http://html5.huceo.com/",tip:"HTML5 技术实现网页"}
+            ,{name:"WiFi Mouse",leftClick:"http://wifimouse.necta.us/#download",tip:"手机充当电脑鼠标"}
+            ,{name:"Kick Ass",leftClick:"https://kickassapp.com/",tip:"脑洞网页消除游戏"}
+            ,{name:"airpano",leftClick:"https://www.airpano.com/",tip:"在线旅游"}
+            ,{name:"AlteredQualia",leftClick:"https://alteredqualia.com/",tip:"头部模型"}
         ]
     ]
 }
