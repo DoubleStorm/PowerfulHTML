1288676dc2af5ba51c87cba67df5fb575d89a810
DESKTOP-9C2C69T\何沁 <2570679716@qq.com>
2020-09-14 18:07:43 +0800

    添加fastSurfing功能页面

---
+++ achievement/computerFunctions/fastSurfing/links.js
@@ -0,0 +1,10 @@
+var theLinks = [
+    // {name:'',url:''},
+    {name:'哔哩哔哩',url:'https://www.bilibili.com/'},
+    {name:'微博',url:'https://weibo.com/login.php'},
+    {name:'知乎',url:'https://www.zhihu.com/signin'},
+    {name:'场库',url:'https://www.vmovier.com/'},
+    {name:'TED',url:'https://www.ted.com/#/'},
+    {name:'4399小游戏',url:'http://www.4399.com/'},
+    {name:'奇奇动漫',url:'https://www.qiqidongman.com/'},
+]
