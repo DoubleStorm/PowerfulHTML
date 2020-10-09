2cfff6df90a55e7340fc74e2d62436936ece7ff8
DESKTOP-9C2C69T\何沁 <2570679716@qq.com>
2020-10-09 18:45:23 +0800

    基本更新

--- achievement/computerFunctions/fastLink2.0/links.js
+++ achievement/computerFunctions/fastLink2.0/links.js
@@ -15,7 +15,6 @@ var theLinks = {
             ,{name:"RxJS 官方教程",leftClick:"https://cn.rx.js.org/manual/overview.html",rightClick:"https://rxjs.dev/guide/overview",tip:"多学学JS异步编程的其他方式（左键中文文档，右键英文文档）"}
             ,{name:"Sass 中文文档",leftClick:"https://www.sass.hk/docs/"}
             ,{name:"node 中文网",leftClick:"https://www.npmjs.cn/"}
-            ,{name:"MDN Web docs",leftClick:"https://developer.mozilla.org/zh-CN/",tip:"源于开发者，服务开发者"}
             ,{name:"CSS 参考手册",leftClick:"http://css.doyoe.com/",tip:"web 前端开发参考手册系列"}
             ,{name:"W3C 官网",leftClick:"https://www.chinaw3c.org/",rightClick:"https://www.w3.org/",tip:"查看 W3C 标准"}
             ,{name:"HTML 字符实体参考",leftClick:"https://www.w3school.com.cn/tags/html_ref_entities.html"}
@@ -31,10 +30,12 @@ var theLinks = {
             ,{name:"BootStrap 图标工具",leftClick:"https://www.runoob.com/try/demo_source/bootstrap-glyph-customization.htm",tip:"快速获得 BootStrap 图标"}
             ,{name:"HTML 字符实体转换",leftClick:"https://c.runoob.com/front-end/691",tip:"快速将 HTML 代码中的特殊符号转换成字符实体"}
             ,{name:"CSS 拾色器",leftClick:"https://www.runoob.com/tags/html-colorpicker.html"}
-            ,{name:"Can I use",leftClick:"https://caniuse.com/",tip:"查看 W3C 标准在各浏览器上的支持程度"}
             ,{name:"Cool Backgrounds",leftClick:"https://coolbackgrounds.io/",tip:"炫酷背景图片生成器"}
             ,{name:"CodePen",leftClick:"https://codepen.io/",tip:"一个在线前端代码编辑器"}
             ,{name:"JS Bin",leftClick:"https://jsbin.com/",tip:"另一个在线前端代码编辑器"}
+            ,{name:"Can I use",leftClick:"https://caniuse.com/",tip:"查看 W3C 标准在各浏览器上的支持程度"}
+            ,{name:"validator",leftClick:"https://validator.w3.org/",tip:"HTML 页面内容验证器"}
+            ,{name:"MDN Web docs",leftClick:"https://developer.mozilla.org/zh-CN/",tip:"源于开发者，服务开发者"}
         ],[
             {name:"BootStrap 模板资源网",leftClick:"http://www.bootstrapmb.com/"}
             ,{name:"iconfont",leftClick:"https://www.iconfont.cn",tip:"阿里管理的矢量图标库"}
@@ -56,6 +57,7 @@ var theLinks = {
             ,{name:"PDF24 Tools",leftClick:"https://tools.pdf24.org/zh/",tip:"多功能 PDF 文件在线编辑器"}
             ,{name:"小鸡词典",leftClick:"https://jikipedia.com/",tip:"速查当下网络流行语"}
             ,{name:"写作猫",leftClick:"https://xiezuocat.com/#/",tip:"简单写作手法提升"}
+            ,{name:"statcounter",leftClick:"https://gs.statcounter.com/",tip:"市场份额统计"}
         ],[
             {name:"HTML5 网页游戏",leftClick:"http://html5.huceo.com/",tip:"HTML5 技术实现网页"}
             ,{name:"WiFi Mouse",leftClick:"http://wifimouse.necta.us/#download",tip:"手机充当电脑鼠标"}
